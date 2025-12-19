530a308bf8794b9a5d7d99c322ca7f19cf04cf14
nato-git <s.f.h.o1219@gmail.com>
2025-12-20 00:54:35 +0900

    スタート画面、細かい部分の修正

--- MainGameScript.py
+++ MainGameScript.py
@@ -1,12 +1,14 @@
 import pygame as pg
 import sys
 
+print(pg.font.get_fonts())
+
 class Character:
   def __init__(self, x, y):
     self.pos = pg.Vector2(x, y)
     self.dir = 0  # 0:下, 1:左, 2:上, 3:右
     self.is_moving = False
-    self.anime_count = 0.0  # アニメーションのコマ送り用
+    self.anime_count = 0.0
     self.charaImg = []
     for i in range(4):
       self.charaImg.append(pg.image.load(
@@ -25,20 +27,18 @@ class Character:
     else:
       self.anime_count = 0
       current_img = self.charaImg[self.dir]
-    screen.blit(current_img, (self.pos.x * dot_size, self.pos.y * dot_size))
+    screen.blit(current_img, (self.pos.x * dot_size,
+                self.pos.y * dot_size - dot_size))
 
   def can_move(self, after_pos, MapObjectList):
-    # 画面範囲内かチェック
     if not (0 <= after_pos.x < 25 and 0 <= after_pos.y < 20):
       return False
-    # 障害物との衝突チェック
     for obj in MapObjectList:
       if after_pos == obj.pos:
         return False
     return True
 
   def update(self, MapObjectList):
-    # 方向と移動量の対応表
     MoveVec = [pg.Vector2(0, 1), pg.Vector2(-1, 0),
                pg.Vector2(0, -1), pg.Vector2(1, 0)]
     keys = pg.key.get_pressed()
@@ -57,58 +57,65 @@ class Character:
         self.is_moving = True
 
 class MapObject:
+  images = {}
+
   def __init__(self, x, y):
     self.pos = pg.Vector2(x, y)
-    self.img = {
-        'test': pg.image.load('./image/object/test.png').convert_alpha(),
-        'floor': pg.image.load('./image/object/floor.png').convert_alpha()
-    }
+
+  @classmethod
+  def load_images(cls):
+    cls.images['test'] = pg.image.load(
+        './image/object/test.png').convert_alpha()
+    cls.images['floor'] = pg.image.load(
+        './image/object/floor.png').convert_alpha()
 
   def draw(self, screen, type):
     dot_size = 32
-    screen.blit(self.img[type], (self.pos.x *
+    screen.blit(self.images[type], (self.pos.x *
                 dot_size, self.pos.y * dot_size))
 
 def main():
   pg.init()
-  dot_size = 32
-  map_size = pg.Vector2(25, 20)
-  screen = pg.display.set_mode(
-      (int(dot_size * map_size.x), int(dot_size * map_size.y)))
-  pg.display.set_caption("Walking Animation Example")
+  screen = pg.display.set_mode((800, 600))
+  pg.display.set_caption("家に帰りたい。")
   clock = pg.time.Clock()
 
-  # オブジェクト生成
-  MainCharacter = Character(5, 5)
-  MapObj = [
-      MapObject(10, 10), MapObject(11, 10), MapObject(12, 10),
-      MapObject(10, 11), MapObject(11, 11), MapObject(12, 11)
-  ]
-  Floor = []
-  for x in range(25):
-    for y in range(20):
-      Floor.append(MapObject(x, y))
+  # 状態管理（0: タイトル, 1: ゲーム本編）
+  state = 0
 
-  running = True
-  while running:
+  MapObject.load_images()
+  player = Character(22, 18)
+  map_objs = [MapObject(10, 10), MapObject(11, 10), MapObject(12, 10)]
+  floors = [MapObject(x, y) for x in range(25) for y in range(8, 20)]
+  font = pg.font.SysFont('mspgothic', 60)
+  while True:
     for event in pg.event.get():
       if event.type == pg.QUIT:
-        running = False
+        pg.quit()
+        sys.exit()
       if event.type == pg.KEYDOWN:
+        if state == 0 and event.key == pg.K_SPACE:
+          state = 1  # ゲーム開始
         if event.key == pg.K_ESCAPE:
-          running = False
-    MainCharacter.update(MapObj)
-    screen.fill((255, 255, 255))
-    for f in Floor:
-      f.draw(screen, 'floor')
-    for obj in MapObj:
-      obj.draw(screen, 'test')
-    MainCharacter.draw(screen)
+          pg.quit()
+          sys.exit()
+    if state == 0:
+      # スタート画面
+      screen.fill((0, 0, 0))
+      text = font.render("家に帰りたい。", True, (255, 255, 255))
+      screen.blit(text, (100, 250))
+    elif state == 1:
+      # ゲーム本編
+      player.update(map_objs)
+      screen.fill((255, 255, 255))
+      for f in floors:
+        f.draw(screen, 'floor')
+      for obj in map_objs:
+        obj.draw(screen, 'test')
+      player.draw(screen)
+
     pg.display.update()
     clock.tick(13)
-
-  pg.quit()
-  sys.exit()
 
 if __name__ == '__main__':
   main()
